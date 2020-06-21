428203b7031af7efa26bf0b26ff1f926ccdaefb6
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-21 17:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 //app.listen(port , () =>{
 //    console.log(`Server running on port : ${port}`);
 //});
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.listen(port, () => {
+    console.log("App is running on port " + port);
 });
 
 app.use(cors());
