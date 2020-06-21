ef477dda0f1770ad20f23e51fa4844bca27d517d
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-21 12:45:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 const express = require('express');
 const cors = require('cors');
 //const mongoose = require('mongoose');
@@ -6,6 +7,10 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.listen(port , () =>{
+    console.log(`Server running on port : ${port}`);
+});
 
 app.use(cors());
 app.use(express.json());
@@ -36,7 +41,3 @@ app.use(function (req, res, next) {
 
 const loginRouter = require('./routes/login');
 app.use('/login', loginRouter);
-
-app.listen(port , () =>{
-    console.log(`Server running on port : ${port}`);
-});
