ce7e98acba9fb21ea0e416a29bd9ce93d155692f
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-21 10:26:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 const loginRouter = require('./routes/login');
 app.use('/login', loginRouter);
 
