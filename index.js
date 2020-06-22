92c843269ba1e63275fa13927f0012bca37bc2c0
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-22 22:08:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ var port = process.env.PORT || 5000;
 // app.use(cors());
 app.use(express.json());
 
-//app.use(function(req, res, next) {
-//    res.header("Access-Control-Allow-Origin", '*');
-//    res.header("Access-Control-Allow-Credentials", true);
-//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//    next();
-//});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //mysql://bfe487f9ef0896:11af1766@us-cdbr-east-05.cleardb.net/heroku_1ae353faf98da68?reconnect=true
 //const con = mysql.createConnection({
