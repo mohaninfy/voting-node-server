2d5a3d00d58a4fdec162e32c81b064a3ef656415
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-22 21:28:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(port, function() {
+    console.log('Running CORS Anywhere on :' + port);
 });
 
